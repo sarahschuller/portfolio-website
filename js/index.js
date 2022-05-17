09829765d5887a0ff7ef8714679da7324c3f8639
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-05-16 21:20:00 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
 const form = document.getElementById("contact-form");
 
-const formEvent = form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let mail = new FormData(form);
-
-    sendMail(mail);
-})
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let mail = new FormData(form);
+  sendMail(mail);
+});
 
 const sendMail = (mail) => {
-
-    fetch("/send", {
-        method: "post",
-        body: mail,
-    }).then((response) => {
-        return response.json();
-    });
+  fetch("/send", {
+    method: "post",
+    body: mail,
+  }).then((response) => {
+    return response.json();
+  });
 };
