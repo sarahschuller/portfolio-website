1be9ea0a190591b736b1ef740543c7e3efe3d325
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-06-02 20:23:59 -0400

    update notes

--- js/skills.js
+++ js/skills.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function(){
   
+/* Delay added to function.
+
+Delay should help so that when a viewer first clicks on the About Me page, a 1.5 second delay begins to allow time to start skimming and scrolling down the page. Delay is not so long as to be extremely noticeable if user refreshes while at the bottom of the page.  
+
+*/
+  
   jQuery('.progress-bar').each(function() {
     jQuery(this).find('.progress-content').delay(1500).animate({
       width:jQuery(this).attr('data-percentage')
